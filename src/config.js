4ac88e827e2806cba6a8a3b5c0df56458ecd2f11
Nico <nicolas.carrasco.m@.com>
2021-06-27 21:19:17 -0400

    mysql hostinger

--- src/config.js
+++ src/config.js
@@ -1,11 +1,9 @@
 import { config as dotenv } from "dotenv";
 dotenv();
 
-console.log(process.env.NICKNAME);
-
 export const config = {
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_DATABASE || "test",
+  host: process.env.DB_HOST || "31.170.166.145",
+  user: process.env.DB_USER || "u524788796_tasks",
+  password: process.env.DB_PASSWORD || "Tasks2021",
+  database: process.env.DB_DATABASE || "u524788796_tasks",
 };
