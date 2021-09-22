d36dadfe2d9cc645ce3f3279698d236dc4a643c3
tsubasaNicolás <36429291+tsubasaNicolas@users.noreply.github.com>
2021-09-22 20:47:44 -0300

    Delete config.js

--- src/config.js
+++
@@ -1,9 +0,0 @@
-import { config as dotenv } from "dotenv";
-dotenv();
-
-export const config = {
-  host: process.env.DB_HOST || "31.170.166.145",
-  user: process.env.DB_USER || "u524788796_tasks",
-  password: process.env.DB_PASSWORD || "Tasks2021",
-  database: process.env.DB_DATABASE || "u524788796_tasks",
-};
